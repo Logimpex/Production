e7ec046b6d62530c56cfcc38fd3c0e165d8b1721
Logimpex <96437237+Logimpex@users.noreply.github.com>
2022-05-18 13:11:05 -0500

    v15.0.0.0.5

--- em_logimpex/__manifest__.py
+++ em_logimpex/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Alfonso Gonzalez (alfonso@ptree.com.mx)",
     'website': "https://ptree.com.mx/",
     'category': 'Customizations',
-    'version': '15.0.0.0.4',
+    'version': '15.0.0.0.5',
     'license': "AGPL-3",
     'sequence': "-80",
     'depends': [
--- em_logimpex/models/__init__.py
+++ em_logimpex/models/__init__.py
@@ -11,3 +11,4 @@ from . import eml_docs
 from . import eml_docs_tags
 from . import project_project
 from . import account_move
+from . import account_incoterms
