ae6ee0a7a8b58e071c1d1f546a1c37ac7129e02a
Logimpex <96437237+Logimpex@users.noreply.github.com>
2022-05-29 22:26:18 -0500

    b15.0.0.1.2

--- em_logimpex/models/res_partner.py
+++ em_logimpex/models/res_partner.py
@@ -7,3 +7,5 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    bank_last_update = fields.Datetime(string='Última Revisión', default=fields.Datetime.now)
+    bank_last_update_by = fields.Many2one('res.users', string='Revisado por', default=lambda self: self.env.user.id)
